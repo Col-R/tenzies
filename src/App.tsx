577d3a5c7e0d01727f3e3aeab197b52b93fd0517
Col-R <2colewestin@gmail.com>
2025-04-18 11:51:59 -0500

    implemented a window resize useEffect, and then a conditional confetti drop. Also a move counter that tells the user how many moves they took to win

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
+import Confetti from 'react-confetti';
 // components
 import Die from './components/Die';
 
 // functions
 import { generateAllNewDice } from './utilities/algorithms';
+import useScreenSize from './utilities/dom';
 
 function App() {
   const [dice, setDice] = useState(generateAllNewDice());
+  const [moves, setMoves] = useState(0);
 
   // toggles isHeld
   function hold(id: string) {
@@ -26,6 +29,7 @@ function App() {
           : { ...die, value: Math.ceil(Math.random() * 6) };
       })
     );
+    setMoves(moves + 1);
   }
 
   // is game won
@@ -37,12 +41,20 @@ function App() {
       console.log('Game won!');
       return true;
     }
+    return false;
   }
   gameWon();
 
+  const { width, height } = useScreenSize();
+
   return (
     <main className='bg-gray-200 mx-auto rounded-lg shadow-lg h-[100%] display flex flex-col justify-center items-center'>
-      Care to shoot some dice?
+      {gameWon() && <Confetti width={width} height={height} />}
+      <p className='text-lg flex justify-center px-5 text-center'>
+        {gameWon()
+          ? `Well played! You got Tenzies in ${moves} moves!`
+          : 'Roll until all dice are the same. Click each die to freeze it at its current value between rolls.'}
+      </p>
       <div className='grid grid-cols-5 gap-4 p-4'>
         {dice.map((die) => (
           <Die
